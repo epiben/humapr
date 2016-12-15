e8e1b1562875a993872384e31ebe0657fd83d680
benskov <benskov@gmail.com>
2016-12-15 22:34:04 +0100

    Tweaking.

--- R/geom_body.R
+++ R/geom_body.R
@@ -4,11 +4,11 @@ GeomBody <- ggproto("GeomBody", Geom,
 
   draw_panel = function(data, panel_scales, coord, coding_scheme) {
     coords <- coord$transform(data, panel_scales)
-    s <- surface_env$surf # The surface_env environment and its surf object are created by humap() before it invoks ggplot
+    s <- humapr_env$surf # The humapr_env environment and its surf object are created by humap() before it invoks ggplot
     for (i in seq(s@summary@numPaths - 1)) {
-      s@paths[[i]]@rgb <- if (!is.na(coords[crosswalk[["simple"]][[i]], "fill"])) coords[crosswalk[["simple"]][[i]], "fill"] else "#FFFFFF"
+      s@paths[[i]]@rgb <- if (!is.na(coords[i, "fill"])) coords[i, "fill"] else "#FFFFFF"
     } # This loops takes less than 0.005 seconds
-    grImport::pictureGrob(body) # Takes about 0.05 seconds
+    grImport::pictureGrob(s) # Takes about 0.05 seconds
   }
 )
 
--- R/humap.R
+++ R/humap.R
@@ -1,80 +1,86 @@
 # \code{humap()} takes relevant specifications from user and uses those to invoke \code{ggplot()}.
 # Output: \code{ggplot2} object that the user may modify with, e.g., \code{theme}.
 
-humap <- function(data, region) {
-  # data = tidy data frame
-  # region = name of columns in data frame that contains counts for each region
-  # tab = logical flag indicating whether input data are already tabulated
+humap <- function(data, var, region = "body", gender = "male", type = "topo",
+                  quality = "normal", side = "front") {
+    # data = tidy data frame
+    # var = name of columns in data frame that contains counts for each region
+    # region = body region, options: body (default), head, neck, arm,
+    # type = topo(graphical), derma(tomes)
+    # gender = n(eutral), f(emale), m(male)
+    # side = front, back, both
+    # quality = draft, normal, high
 
-  surface_env <- new.env(parent = .GlobalEnv)
-  surface_env$surf <- grImport::readPicture("data/body_male_front.xml")
-    # This needs to take into account further specification from the user
+    # Set up dedicated environment for the humap, so it's easy to call objects
+    # and values within the geom functions
+        humapr_env <<- new.env(parent = emptyenv())
 
-  # Defining crosswalk between various coding systems and ids of pictureGrob shapes.
-  # For now, only AIS coding is supported but implemented in list form to facilitate potential extension to other
-  # classification schemes later on
-  # This crosswalk thing should probably be in the humap() call instead, so it's just done once...
-  crosswalk <- list(
-      ais = c( # Ensure to make it a named vector (names are path ids)
-          51, # Scalp
-          52, # Forehead
-          53, # Face
-          54, # Eye
-          55, # Eyelid
-          56, # Ear
-          57, # Nose
-          58, # Lip
-          59, # Neck
-          60, # Shoulder
-          61, # Arm (upper)
-          62, # Elbow
-          63, # Forearm
-          64, # Wrist
-          65, # Hand
-          66, # Finger
-          # Theres's no LOC = 67 in the AIS scheme
-          68, # Back
-          # Theres's no LOC = 69 in the AIS scheme
-          70, # Chest
-          71, # Abdomen
-          72, # Buttocks
-          73, # Genitalia
-          74, # Perineum
-          75, # Hip
-          76, # Thigh
-          77, # Knee
-          78, # Leg
-          79, # Ankle
-          80, # Foot
-          81), # Toe
-      simple = c( # Based on data from my thesis, mainly for testing purposes but might be useful also "post-publication"
-          11, # Head, frontside
-          12, # Head, posterior side
-          21, # Neck, frontside
-          22, # Neck, posterior side
-          31, # Chest, frontside
-          32, # Chest, posterior side
-          41, # Abdomen, frontside
-          42, # Abdomen, posterior side
-          51, # Pelvic area, frontside
-          52, # Pelvic area, posterior side
-          61, # Upper arm, frontside
-          62, # Upper arm, posterior side
-          64, # Forearm, frontside
-          65, # Forearm, posterior side
-          71, # Palm
-          72, # Back of hand
-          81, # Legs and feet, frontside
-          82) # Legs and feet, posterior side
-  )
-  names(crosswalk$simple) <- c(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9)
+    # Import appropriate XML file
+        geom_file <- paste(paste(type, region, gender, side, quality, sep = "_"), "xml", sep = ".")
+        humapr_env$surf <- grImport::readPicture(paste0("data/", geom_file))
 
-  # User may supply a function that changes (i.e., simplifies) the coding, e.g., merging facial sub-regions
-  if (exists(surface_env$simplify_coding)) crosswalk[surface_env$simplify_coding[1]] <- surface_env$simplify_coding[2]
+    # Coding of body regions
+        # Crosswalks between coding systems and pictureGrob shape id's
+        crosswalk <- list(
+            ais = c( # Ensure to make it a named vector (names are path ids)
+                51, # Scalp
+                52, # Forehead
+                53, # Face
+                54, # Eye
+                55, # Eyelid
+                56, # Ear
+                57, # Nose
+                58, # Lip
+                59, # Neck
+                60, # Shoulder
+                61, # Arm (upper)
+                62, # Elbow
+                63, # Forearm
+                64, # Wrist
+                65, # Hand
+                66, # Finger
+                # Theres's no LOC = 67 in the AIS scheme
+                68, # Back
+                # Theres's no LOC = 69 in the AIS scheme
+                70, # Chest
+                71, # Abdomen
+                72, # Buttocks
+                73, # Genitalia
+                74, # Perineum
+                75, # Hip
+                76, # Thigh
+                77, # Knee
+                78, # Leg
+                79, # Ankle
+                80, # Foot
+                81), # Toe
+            simple = c( # Based on data from my thesis, mainly for testing purposes but might be useful also "post-publication"
+                11, # Head, frontside
+                12, # Head, posterior side
+                21, # Neck, frontside
+                22, # Neck, posterior side
+                31, # Chest, frontside
+                32, # Chest, posterior side
+                41, # Abdomen, frontside
+                42, # Abdomen, posterior side
+                51, # Pelvic area, frontside
+                52, # Pelvic area, posterior side
+                61, # Upper arm, frontside
+                62, # Upper arm, posterior side
+                64, # Forearm, frontside
+                65, # Forearm, posterior side
+                71, # Palm
+                72, # Back of hand
+                81, # Legs and feet, frontside
+                82) # Legs and feet, posterior side
+        )
+        names(crosswalk$ais) <- c()
+        names(crosswalk$simple) <- c(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9)
 
-  pars <- as.character(structure(as.list(match.call())[-1], class = "uneval")) # => user may supply parameters w/o quotation marks
+        # User may supply a function that changes (i.e., simplifies) the coding, e.g., merging facial sub-regions
+        if (exists("humapr_env$simplify_coding")) crosswalk[humapr_env$simplify_coding[1]] <- humapr_env$simplify_coding[2]
 
-  ggplot(data, aes_string(x = pars["region"], fill = "-..count..")) +
-    geom_body()
-    # + theme(...) at some point
+    ggplot(data, aes_string(x = var, fill = "-..count..")) +
+        geom_body()
+        # + theme(...) at some point
 }
