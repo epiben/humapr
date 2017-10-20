1486b44b0132398d88456423429f5b9f796bb6e4
Benjamin Skov Kaas-Hansen <benskov@gmail.com>
2017-10-21 00:32:21 +0200

    Include simple bridge, for my thesis data

--- R/build_bridge.R
+++ R/build_bridge.R
@@ -1,4 +1,4 @@
-build_bridge <- function (d, bridge, type, lr.var, loc.var) {
+build_bridge <- function (d, bridge, type) {
     if (is.character(bridge)) {
         if (bridge == "ais") {
             if (!is.list(h_env$lr.var)) { # If list, user should have given correct info
@@ -10,19 +10,24 @@ build_bridge <- function (d, bridge, type, lr.var, loc.var) {
     }
 
     built_in <- list(
-        ais = list(head = 51:58, neck = 59, shoulder = 60, arm = 61, elbow = 62,
-                   forearm = 63, wrist = 64, hand = 65, fingers = 66, back = 68,
-                   flank = 69, chest = 70, abdomen = 71, buttocks = 72,
-                   genitalia = 73:74, hip = 75, thigh = 76, knee = 77, leg = 78,
-                   ankle = 79, foot = 80, toes = 81))
+        ais = list(
+            head = 51:58, neck = 59, shoulder = 60, arm = 61, elbow = 62,
+            forearm = 63, wrist = 64, hand = 65, fingers = 66, back = 68,
+            flank = 69, chest = 70, abdomen = 71, buttocks = 72,
+            genitalia = 73:74, hip = 75, thigh = 76, knee = 77, leg = 78,
+            ankle = 79, foot = 80, toes = 81),
+        simple = list(
+            head = 11:12, neck = 21:22, chest = 31:32, abdomen = 41:42,
+            pelvis = 51:52, arm = 61:62, forearm = 64:65, hand = 71:72,
+            leg = 81:82)
+    )
 
-    if (names(built_in) %in% bridge) bridge <- built_in[[bridge]]
+    if (bridge %in% names(built_in)) bridge <- built_in[[bridge]]
 
-    if (!is.list(bridge)) stop(paste0("Please, make sure to give a *list* as
-                                      the 'bridge' argument, or choose one of
-                                      the built-in options: ", names(built_in),
-                                      "."),
-                               call. = FALSE)
+    if (!is.list(bridge))
+        stop(paste0("Please, make sure to give a *list* as the 'bridge' argument, or choose one of the built-in options: ",
+                    names(built_in), "."),
+             call. = FALSE)
 
     for (hreg in names(bridge)) {
         d[d[, h_env$loc.var] %in% bridge[[hreg]], h_env$loc.var] <- hreg
