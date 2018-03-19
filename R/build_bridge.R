b81502a4005d8bd7239f9bfceb0bb76fb4e6b3ef
Benjamin Skov Kaas-Hansen <benskov@gmail.com>
2018-03-19 15:50:00 +0100

    Include built-in conversion of L1 to left/right

--- R/build_bridge.R
+++ R/build_bridge.R
@@ -1,6 +1,6 @@
 build_bridge <- function (d, bridge, type) {
     if (is.character(bridge)) {
-        if (bridge == "ais") {
+        if (bridge %in% c("ais", "ais_simple")) {
             if (!is.list(h_env$side)) { # If list, user should have given correct info
                 h_env$side <- list(var = h_env$side, left = c(2, 20:29),
                                      right = c(1, 10:19), mid = c(0, 3))
