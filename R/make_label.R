3df3200dc522bb3070d738b80ae5bf710d26a6a1
Benjamin Skov Kaas-Hansen <benskov@gmail.com>
2018-03-19 09:13:33 +0100

    Add documentation, consolidate code

--- R/make_label.R
+++ R/make_label.R
@@ -1,4 +1,15 @@
-make_label <- function (id, data, local_coords, label_pad) {
+#' Create textGrob for humap annotation
+#'
+#' Internal function called within \code{geom_humap}. I cannot think of any
+#' scenario in which a user would need to call this function.
+#'
+#' @param id internal id (e.g., right_abdomen) of label for which to create
+#'   textGrob
+#' @param data the original data sent into \code{humap} by \code{ggplot2}
+#' @param local_coords data fram holding coordinates for all annotations;
+#'   \code{id} is used to pick appropriate coordinates
+
+make_label <- function (id, data, local_coords) {
     # The data input is the label_data data frame (= original data produced by ggplot)
     # Temporary id, tid
     tid <- if (!h_env$half == "mirror") id else substring(id, regexpr("_", id) + 1)
@@ -28,6 +39,7 @@ make_label <- function (id, data, local_coords, label_pad) {
         dplyr::select(y1, x2, side)
     x <- grid::unit(coords$x2, "native") +
         grid::unit(1, "strwidth", "  ") * if (coords$side == "left") 1 else -1
+            # ensure distance to annotation line
     y <- grid::unit(coords$y1, "native") + grid::unit(0.1, "lines")
 
     grid::textGrob(label, x = x, y = y, default.units = "native", just = coords$side)
