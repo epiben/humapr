40999b80b9a5b8c6baa73be6b2f88ab9adb73485
Benjamin Skov Kaas-Hansen <benskov@gmail.com>
2018-03-19 13:22:58 +0100

    Simplify code

--- R/prep_annotations.R
+++ R/prep_annotations.R
@@ -11,8 +11,10 @@ prep_annotations <- function(mapped_loc, combine, type, gender, proj, body_halve
         h_env$anno_coords <- h_env$mapdf %>%
             dplyr::group_by(id) %>%
             dplyr::summarise(
-                x0 = mean(c(max(long), min(long))),
-                y0 = mean(c(max(lat), min(lat)))
+                # x0 = mean(c(max(long), min(long))),
+                # y0 = mean(c(max(lat), min(lat)))
+                x0 = mean(range(long)),
+                y0 = mean(range(lat)))
             ) %>%
             dplyr::rename(region = id) %>%
             as.data.frame()
