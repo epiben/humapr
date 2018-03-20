37ceec78164f1486b7cfc20632d82547224425cb
Benjamin Skov Kaas-Hansen <benskov@gmail.com>
2018-03-20 14:54:46 +0100

    Improve documentation of humap_body()

--- R/humap_body.R
+++ R/humap_body.R
@@ -1,53 +1,81 @@
 #' Create a humap body plot
 #'
-#' \code{humap_body} creates a ggplot object, using its own set of "geoms", i.e.,
-#' human body oulines on which your data are projected in a heatmap-like
-#' fashion. \code{humapr} is an extension of ggplot2, and objects created with
-#' the \code{humap_*} functions can be modified with standard ggplot2
-#' setting--e.g., calls to \code{theme()}.
+#' \code{humap_body} creates a choropleth body map, on which your data are
+#' projected. \code{humapr} is an extension of ggplot2, so \code{humap_*}
+#' objects are compatible with with standard \code{ggplot2} function, e.g.,
+#' \code{theme()} and \code{facet_wrap()}.
 #'
-#' If you don't supply a \code{body_halves} argument humapr will assume default
-#' to \code{pool} mode.
+#' If you don't supply a \code{body_halves} argument humapr will default to
+#' \code{pool} mode.
 #'
-#' \code{controls} is a list of more specific parameters to modify details in
-#' the apperance of the humap. These controls are currently available:
-#' \code{vert_adj} has "minimal" as default, but may also be set to "even" for
-#' vertical even distribution, or "smart" to avoid crossing annovation lines.
-#' \code{na_fill} specificies the fill of zero-count regions. \code{mid_include}
-#' is a logical indicating whether midline observations should be included;
-#' currently, if this is set to TRUE, a midline observation will count as half
-#' an observations on each side. Default is FALSE.
+#' The \code{annotate} argument may be a named \code{list} with any combination
+#' of the following four elements. The \code{details} element should be a
+#' character string, and may be any of the valid values of the simple argument
+#' (\code{"freq"}, \code{"all"}, \code{NA}). The remaining three elements
+#' \code{gp}, \code{gp_text} and \code{gp_lines} must be \code{gpar} objects
+#' (see \code{?grid::gpar}). If only \code{gp} is specified, it is applied to
+#' both annotation text and lines. \code{gp_text} and \code{gp_lines} are
+#' applied to annotation text and lines individually.
 #'
-#' @param data tidy data frame (more under details)
-#' @param loc column in data frame containing localisation codes for
-#'   observations. Character string.
-#' @param side variable indication left and right side of observations. Values
-#'   must be "left" or "right". May also be a named *list* that defines the
-#'   variable (= data frame column) holding the variables, and which value(s) in
-#'   that column correspond(s) to left and right. Valid specifications are,
-#'   i.a., \code{side = "xx"} and \code{side = list(var = "xx", left = c("left",
-#'   "l"), right = "r")}
-#' @param type simple, (default), f(emale), m(male)
-#' @param proj "front" or "back". Ignored if \code{type = "simple"}.
+#' The \code{bridge} argument allows you to use your own localisation values,
+#' and bridge them to those of \code{humap_body}. The idea is simple: you supply
+#' a named \code{list}, each element of which corresponds one region in the
+#' \code{humap_body} map; the name of the element should be the name of the
+#' region, and the element should be a character vector of values in your data
+#' that correspond to that region. See vignette [add reference to vignette here]
+#' for examples.
+#'
+#' If you want to combine several regions and map them as one, supply a named
+#' \emph{list} in \code{combine}, following this logic: the name of each element
+#' will be printed as the annotation text (if you so desire), and the element
+#' must be a character vector specifying the names of \code{humap_body} regions
+#' to map as one. Underscores in the list element names will be converted to
+#' wide spaces. Make sure to \emph{not} use a name for a merged region that is
+#' already used for another region; e.g., this is not allowed, as \code{hand}
+#' already the name of another region: \code{list(hand = c("shoulder", "arm",
+#' "elbow", "wrist"))}. You may, however, use a name of a region inside the
+#' group, so this would be allowed: \code{list(hand = c("wrist", "hand"))}.
+#'
+#' @param data tidy data frame, like the type you'd feed into ggplot() when
+#'   producing a histogram
+#' @param loc character string indicating the name of the data frame column
+#'   containing localisation codes for observations.
+#' @param side either character string or list. If \emph{string}, the argument
+#'   should be the data frame column indicating left and right side of
+#'   observations, and column values must be "left" or "right". If \emph{list},
+#'   the \code{var} element must denote the name of the data frame column
+#'   holding the data, and two \code{left} and \code{right} elements should be
+#'   character vectors denoting what values in the data frame column correspond
+#'   to those sides. Thus, for a data frame with the relevant column called
+#'   'lr_col', valid specifications would include \code{"lr_col"} and
+#'   \code{list(var = "lr_col", left = c("left", "l"), right = c("right",
+#'   "r"))}.
+#' @param type currently, only \code{"simple"} is available, but we're working
+#'   on more natural-looking and gender-specific maps as well.
+#' @param proj \code{"front"} or \code{"back"}. Ignored if \code{type =
+#'   "simple"}, so currently not in use.
 #' @param body_halves character string defining how to deal with body halves.
-#'   "separate" (default) discriminates the left half from the right; "join"
-#'   merges observations in, e.g., right and left side of the chest.
-#' @param annotate \code{freq} (defaults) shows only absolute and relative
-#'   frequencies. \code{all} include region names. \code{NA} omits labels
-#'   altogether. More elaborate argument may be given as a named \code{list},
-#'   consisting of a \code{details} element (optional, but can be any of the
-#'   values for the string argument mode) and a \code{gp} element holding a
-#'   \code{grid::gpar}-object (see ?grid::gpar)
+#'   \code{"separate"} (default) discriminates the left half from the right;
+#'   \code{"join"} merges observations in, e.g., right and left side of the
+#'   chest.
+#' @param annotate \code{"freq"} (defaults) shows only absolute and relative
+#'   frequencies, \code{"all"} includes region names, and \code{NA} omits labels
+#'   altogether. See Details for ways to fine-tune the apperance of annotations.
 #' @param combine a \emph{list} of vectors naming the regions to be combined and
-#'   mapped as one. E.g., \code{list(arm = c("shoulder", "arm", "elbow",
-#'   "wrist"))}
-#' @param bridge used to bridge between the user's data format and the native
-#'   format of humapr. Further explanations to follow
-#' @param na_rm should missing data be removed? Defaults is \code{FALSE}.
-#' @param controls list of controls for fine-tuning. See details.
+#'   mapped as one, e.g., \code{list(arm = c("shoulder", "arm", "elbow",
+#'   "wrist"))}. See Details.
+#' @param bridge named \emph{list} specifying the bridge from your data format
+#'   to the native format of \code{humap_body}, e.g., \code{list(head =
+#'   c("head", "face", "scalp"))}. See Details.
+#' @param na_rm logical indicating whether to remove missing data. Default is
+#'   \code{FALSE}.
+#' @param controls named \emph{list} of more specific parameters for fine-tuning
+#'   the appereance of the humap. Currently, only one control is available (more
+#'   will follow in the fuuture): \code{na_fill} specificies the fill of
+#'   zero-count regions (default: "black").
 #'
 #' @return A ggplot object with suitable layout settings for the purpose of
-#'   \code{body}.
+#'   \code{hump_body}.
 #'
 #' @export
 #' @import ggplot2
@@ -56,7 +84,7 @@
 
 humap_body <- function(data, loc, side = NULL, type = "simple", proj = "neutral",
                        body_halves = "separate", annotate = "freq", bridge = NULL,
-                       na_rm = FALSE, combine = NULL, controls = NULL) {
+                       combine = NULL, na_rm = FALSE, controls = NULL) {
 
     # Safety moves and housekeeping
     if (missing(data)) stop("Please, include data.")
