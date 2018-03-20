64f167430c86fb731d480748a0faab325f3e2bc8
Benjamin Skov Kaas-Hansen <benskov@gmail.com>
2018-03-20 11:13:52 +0100

    Try chained %||%

--- R/housekeeping.R
+++ R/housekeeping.R
@@ -23,7 +23,7 @@ housekeeping <- function(user, defs) {
     valid_annotate <- c("freq", "all", NA, NULL)
     h_env$gp <- h_env$gp_text <- h_env$gp_lines <- grid::gpar(col = "black", fontsize = 9)
     if (is.list(h_env$annotate)) {
-        h_env$gp_text <- h_env$annotate$gp_text %||% h_env$gp
+        h_env$gp_text <- h_env$annotate$gp_text %||% h_env$annotate$gp %||% h_env$gp
         h_env$gp_lines <- h_env$annotate$gp_lines %||% h_env$gp
         h_env$annotate <- h_env$annotate$detail %||% "freq" # give default
     }
