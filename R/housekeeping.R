5fcfdb3a6e4bfa5bfcd341239c7ed65d58ec179c
Benjamin Skov Kaas-Hansen <benskov@gmail.com>
2018-03-20 11:37:58 +0100

    Fix annotation gp's

--- R/housekeeping.R
+++ R/housekeeping.R
@@ -25,7 +25,7 @@ housekeeping <- function(user, defs) {
     # h_env$gp <- h_env$gp_text <- h_env$gp_lines <- grid::gpar(col = "black", fontsize = 9)
     h_env$gp <- grid::gpar(col = "black", fontsize = 9)
     if (is.list(h_env$annotate)) {
-        h_env$gp <- h_env$annotate$gp %||% h_env$gp
+        h_env$gp <- h_env$annotate[["gp"]] %||% h_env$gp # indexing to ensure gp_* isn't used
         h_env$gp_text <- h_env$annotate$gp_text %||% h_env$gp
         h_env$gp_lines <- h_env$annotate$gp_lines %||% h_env$gp
         h_env$annotate <- h_env$annotate$detail %||% "freq" # give default
